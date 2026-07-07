1f5c15f31ad746f3e714904b9364fd889a0ebe2b
MatemoDeMatemo <mateusz.draga.08@gmail.com>
2026-07-07 16:49:36 +0200

    Wykres z 15 dni ready

--- Main.py
+++ Main.py
@@ -6,60 +6,79 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.dates as mdates
 
-from collections import defaultdict # sklejanie
-
-pd.set_option("display.float_format", "{:.0f}".format) # time format
+pd.set_option("display.float_format", "{:.2f}".format)
 pd.set_option("display.max_columns", 5)
 
 
 ## Path/Import
 path_desktop = Path.home() / "Desktop"
-
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "Test_Shp" / "Azolla_Detections_as_Polygons_0502_2019.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_2602_2019_1" / "Azolla_Detections_as_Polygons_2602_2019_1.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_25-10_05" / "GEE_Azolla_Polygons_2602_2019_9_25-10_05.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_2_months" / "Azolla_Detection_1103_2019_0501.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_2_months" / "Azolla_Detection_1103_2019_2709.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_2_months" / "Azolla_Detection_1103_2019_benchmarkweek_otherclass.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_2_months" / "Azolla_Detection_1103_2019_benchmarkweek_otherclass_6bands.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_Folder" / "AV_0318_2025_09.shp"
-#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_Folder_WY" / "AV_20_03_2025_18-03_AV_Classifier_B8_B6_B7_B8A_NDVI.shp"
-shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_Folder_WY" / "2026-05-08_detect-2025-01-01_to_2026-01-01_clf-AV_Classifier_2026-05-08_n6_B5_B6_B7_B8_B8A_NDVI.geojson"
+# File import - zawiera polygony z Azolla z roznych dat. Wiele polygonow dla jednego dnia.
+shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_Folder_WY" / "AV_2025_full_year" / "AV_20_03_2025_18-03_AV_Classifier_B8_B6_B7_B8A_NDVI.shp"
+#shp_path = path_desktop / "GEE_Azolla_Materials" / "GEE_Folder_WY" / "2026-05-08_detect-2025-01-01_to_2026-01-01_clf-AV_Classifier_2026-05-08_n6_B5_B6_B7_B8_B8A_NDVI.geojson"
 
 # Load the data
 gdf = gpd.read_file(shp_path)
+print(gdf.columns)
 
-# Sort by time and polygon size
-# gdf_sorted = gdf.sort_values(by=["system:tim", "count"], ascending= [True, False])
-# print("gdf_sorted: ", gdf_sorted)
-#
-# # Change time type
-# gdf_sorted["date"] = pd.to_datetime(
-#     gdf_sorted["system:tim"],
-#     unit="ms"
-#     ).dt.floor("s")
-
-# json
-# Posortuj cale dane, najpierw po czasie, potem liczbie pixeli na poligon (od najwiekszego)
+# Sort the polygons
 gdf_sorted = gdf.sort_values(
-    by=["system:time_start", "count"],
+    by=["system:tim", "count"],
     ascending=[True, False]
 )
-print(gdf_sorted)
 
-# Zamiana czasu z ciagu cyfr na daty
-gdf_sorted["date"] = pd.to_datetime(
-    gdf_sorted["system:time_start"],
-    unit="ms"
-).dt.floor("s")
+# Change date format
+gdf_sorted["date"] = pd.to_datetime(gdf_sorted["system:tim"],unit="ms").dt.floor("s")
+gdf_sorted["date_ymd"] = pd.to_datetime(gdf_sorted["system:tim"],unit="ms").dt.date
+
+# Print the polygons
+print("gdf_sorted: \n", gdf_sorted[["image_id", "count", "date", "date_ymd"]])
+
+## Group by date
+
+count_per_day = (
+    gdf_sorted
+    .groupby("date")["count"]
+    .sum()
+    .reset_index(name="count_sum")
+)
+
+count_per_day["hectars"] = count_per_day["count_sum"].astype(float) * 0.01
+print("count_per_day: \n", count_per_day)
+
+## Stats by a day
+
+daily_stats = (
+    gdf_sorted
+    .groupby("date")["count"]
+    #.groupby("date_ymd")["count"]
+    .agg(
+        count_sum="sum",
+        n_polygons="count",
+        max_polygon="max"
+    )
+    .reset_index()
+)
+
+print(daily_stats.to_string(index=False))
+
+
+
+#### Plot 1 - Timescale
 
 
 
 ### Wykresimport matplotlib.dates as mdates
 N_DAYS = 15
 
-gdf_sorted_inx = gdf_sorted.set_index("date")
-rolling_max = gdf_sorted_inx["count"].resample(f"{N_DAYS}D").max()
+count_per_day = count_per_day.set_index("date") # make "date" an index
+rolling_max = (
+    count_per_day["hectars"]
+    .resample(f"{N_DAYS}D")  # divide the data by N_DAYS
+    .max()
+)
+print("rolling_max: \n", rolling_max)
+# Fig maker
+
 
 fig, ax = plt.subplots(figsize=(10, 5))
 
@@ -67,20 +86,56 @@ rolling_max.plot(
     ax=ax,
     marker="o",
     linewidth=1.5,
-    markersize=4
+    markersize=4,
+    color="#2E7D32"
 )
 
-# ---- TUTAJ DODAJ ----
-
-ticks = pd.date_range(
+# główne ticki - pierwszy dzień miesiąca
+ticks_month = pd.date_range(
     start="2025-01-01",
-    end="2025-12-31",
+    end="2025-12-01",
     freq="MS"
 )
 
+# pomocnicze ticki - połowa miesiąca
+ticks_mid = pd.to_datetime([
+    "2025-01-15",
+    "2025-02-15",
+    "2025-03-15",
+    "2025-04-15",
+    "2025-05-15",
+    "2025-06-15",
+    "2025-07-15",
+    "2025-08-15",
+    "2025-09-15",
+    "2025-10-15",
+    "2025-11-15",
+    "2025-12-15"
+])
 
-ax.set_xticks(ticks)
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%b\n%Y"))
+
+# ustaw główne ticki
+ax.set_xticks(ticks_month)
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+
+# ustaw pomocnicze ticki
+ax.set_xticks(ticks_mid, minor=True)
+
+ax.tick_params(
+    axis="x",
+    which="minor",
+    length=4
+)
+
+ax.tick_params(
+    axis="x",
+    which="major",
+    length=7
+)
+
+
+#ax.minorticks_off() # turn off minor ticks
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
 
 ax.set_xlim(
     pd.Timestamp("2025-01-01"),
@@ -90,18 +145,85 @@ ax.set_xlim(
 # ---------------------
 
 ax.set_title(
-    f"Maximum detections every {N_DAYS} days",
-    fontweight="bold",
+    r"$\it{Azolla\ filiculoides}$ coverage dynamics in the Tagus River (2025)",
+    #fontweight="bold",
     fontsize=14
 )
 
 ax.set_xlabel("Date")
-ax.set_ylabel("Maximum count")
+ax.set_ylabel(r"$\it{Azolla\ filiculoides}$ coverage area (ha)")
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
 
-plt.xticks(rotation=45)
+ax.grid(
+    True,
+    linestyle="--",
+    alpha=0.3
+)
+
+plt.xticks(rotation=0)
+ax.set_yticks(range(0, 101, 10))
 plt.tight_layout()
 plt.show()
 
+## 2 plot
+# ustawienie daty jako indeks
+
+
+count_per_day2 = (
+    gdf_sorted
+    .groupby("date")["count"]
+    .sum()
+    .reset_index(name="count_sum")
+)
+count_per_day2["date_ymd"] = pd.to_datetime(count_per_day2["date"]).dt.date
+
+print("cpr2.1: \n", count_per_day2)
+
+count_per_day2 = (
+    count_per_day2
+    .groupby("date_ymd")["count_sum"]
+    .max()
+    .reset_index(name="count_sum")
+)
+print("cpr2.2: \n", count_per_day2)
+count_per_day2 = count_per_day2.set_index("date_ymd")
+
+fig, ax = plt.subplots(figsize=(10, 5))
+
+count_per_day2["count_sum"].plot(
+    ax=ax,
+    marker="o",
+    linewidth=1.5,
+    markersize=4
+)
+
+ticks = pd.date_range(
+    start="2025-01-01",
+    end="2025-12-31",
+    freq="MS"
+)
+
+ax.set_xticks(ticks)
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+
+ax.set_xlim(
+    pd.Timestamp("2025-01-01"),
+    pd.Timestamp("2025-12-31")
+)
+
+ax.set_title(
+    "Daily Azolla detections",
+    fontweight="bold",
+    fontsize=14
+)
+
+ax.set_xlabel("Months")
+ax.set_ylabel("Count")
+
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
 
 #
 # ### Heat map ####
@@ -193,3 +315,5 @@ plt.show()
 # plt.colorbar(label=f"Overlaps ({day})")
 # plt.title("Daily Heatmap")
 # plt.show()
+
+
